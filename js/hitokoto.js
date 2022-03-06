4c74f7a15c4833428e02d14d3e8ea82f3269c366
guest997 <zyp1036635267@gmail.com>
2022-03-06 11:13:56 +0800

    Site updated: 2022-03-06 11:13:50

--- js/hitokoto.js
+++
@@ -1,8 +0,0 @@
-function getHitokoto() {
-    $.get('https://v1.hitokoto.cn/?encode=json&c=a', function (response) {
-        let hitokoto = response.hitokoto;
-        let from = response.from;
-        $('#hitokoto').html(hitokoto + ' —《' + from + '》');
-    });
-}
-setInterval(getHitokoto, 3000);
